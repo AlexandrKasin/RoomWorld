5aecf55db3758129b95c0f23d36ff335f7fcd1df
Aliaksandr Kasin <Aliaksandr.Kasin@itechart-group.com>
2018-08-07 15:33:53 +0300

    Add project files.

---
+++ RoomWorld/Controllers/AccountController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using RoomWorld.Models;
+
+namespace RoomWorld.Controllers
+{
+    public class AccountController : Controller
+    {
+        private List<User> users = new List<User>
+        {
+           new User { email = "sasha@mail.ru", name = "sasha", password = "123", role = "user", surname = "K", phoneNumber = "+3751234596" }
+        };
+
+        [HttpGet("/token")]
+        public async Task Token()
+        {
+            var username = "sasha@mail.ru";
+            var password = "123w";
+
+            var identity = GetIdentity(username, password);
+            if (identity == null)
+            {
+                Response.StatusCode = 400;
+                await Response.WriteAsync("Invalid username or password.");
+                return;
+            }
+
+            var now = DateTime.UtcNow;
+            var jwt = new JwtSecurityToken(
+                    issuer: AuthOptions.ISSUER,
+                    audience: AuthOptions.AUDIENCE,
+                    notBefore: now,
+                    claims: identity.Claims,
+                    expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                    signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var response = new
+            {
+                access_token = encodedJwt,
+                username = identity.Name
+            };
+
+            Response.ContentType = "application/json";
+            await Response.WriteAsync(JsonConvert.SerializeObject(response, new JsonSerializerSettings { Formatting = Formatting.Indented }));
+        }
+
+        private ClaimsIdentity GetIdentity(string username, string password)
+        {
+            User user = users.FirstOrDefault(x => x.email == username && x.password == password);
+            if (user != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, user.email),
+                    new Claim(ClaimsIdentity.DefaultRoleClaimType, user.role)
+                };
+                ClaimsIdentity claimsIdentity =
+                new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                    ClaimsIdentity.DefaultRoleClaimType);
+                return claimsIdentity;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ RoomWorld/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoomWorld.Models
+{
+    public class User
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string role { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string phoneNumber { get; set; }
+    }
+}
